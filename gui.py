2ed5402b0a784ec166b4f4acf8259ab484db659f
Adnan525 <adnaan525@gmail.com>
2022-10-24 23:49:25 +1100

    run button added, enables when all checks passed

--- gui.py
+++ gui.py
@@ -16,47 +16,56 @@ selectDatasetLabel = tk.Label(text="Select a dataset: ")
 selectDatasetLabel.place(x = 100, y = 20)
 
 dataset = tk.StringVar()
-selectedDatasetLabel = tk.Label(text = "")
+selectedDatasetLabel = tk.Label(text = "", fg= "#ff0000")
 selectedDatasetLabel.place(x = 100, y = 80)
 
-def foo():
+def onClickDFRB():
     global targetDF
-    selectionMessage = f"selected dataset is {dataset.get()}."
+    global isDataLoaded
+    selectionMessage = f"Selected dataset is {dataset.get()}."
     selectedDatasetLabel.config(text = selectionMessage)
     targetDF = dataset.get()
+    isDataLoaded = True
+
+#adding radio buttons
+#value will be just small letter
 irisRB = tk.Radiobutton(
     text="Iris",
     variable = dataset,
     value='iris',
-    command=foo
+    command=onClickDFRB
 )
 irisRB.place(x=100, y=50)
+
 breastCancerRB = tk.Radiobutton(
     text = "Breast Cancer",
     variable = dataset,
     value = "breast-cancer",
-    command = foo
+    command = onClickDFRB
 )
 breastCancerRB.place(x=200, y=50)
 wineRB = tk.Radiobutton(
     text = "Wine",
     variable = dataset,
     value = "wine",
-    command = foo
+    command = onClickDFRB
 )
 wineRB.place(x = 400, y = 50)
 
+#classification algorithm
 selectClassifierLabel = tk.Label(text = "Select a classifier: ")
 selectClassifierLabel.place(x = 100, y = 120)
 
 classAlgName = tk.StringVar()
-selectedAlgorithmLabel = tk.Label(text = "")
+selectedAlgorithmLabel = tk.Label(text = "", fg= "#ff0000")
 selectedAlgorithmLabel.place(x = 100, y  = 170)
 def onClickClassificationRB():
     global targetModel
+    global isClassSelected
     selectionMessage = f"{classAlgName.get()} algorithm selected."
     selectedAlgorithmLabel.config(text=selectionMessage)
     targetModel = classAlgName.get()
+    isClassSelected = True
 
 knnRB = tk.Radiobutton(
     text="KNN",
@@ -65,6 +74,7 @@ knnRB = tk.Radiobutton(
     command=onClickClassificationRB
 )
 knnRB.place(x=100, y=150)
+
 supportVectorRB = tk.Radiobutton(
     text = "Support Vector Classification",
     variable = classAlgName,
@@ -84,10 +94,10 @@ fold = tk.StringVar(value="3") #can extract only string from entry
 foldLabel = tk.Label(text= "number of folds : ")
 foldLabel.place(x = 100, y = 200)
 
-checkMessageDefault = "no check has been done yet"
+checkMessageDefault = "No check has been done yet"
 checkMessage = tk.Text()
 checkMessage.insert("end", checkMessageDefault)
-checkMessage.place(width=500, height = 100)
+checkMessage.place(width=500, height = 200)
 checkMessage.place(x=100, y=250)
 
 def check():
@@ -95,15 +105,19 @@ def check():
     global targetModel
     global isFoldCorrect
     checkMessage.delete("1.0", "end")
+    checkMessage.insert("end", "======================================\n")
     if int(int(fold.get())) <= 1:
-        checkMessage.insert("end", f"Fold number has to be greater than 1, you selected {fold.get()}")
+        checkMessage.insert("end", f"Fold number has to be greater than 1, you selected {fold.get()}\n")
         isFoldCorrect = False
     else:
         checkMessage.insert("end", f"{targetDF} dataset selected.\n")
         checkMessage.insert("end", f"{targetModel} algorithm selected.\n")
-        checkMessage.insert("end", f"{fold.get()} fold selected.")
+        checkMessage.insert("end", f"{fold.get()} fold selected.\n")
         isFoldCorrect = True
-
+    checkMessage.insert("end", "======================================")
+    #activate run button
+    if isClassSelected and isDataLoaded and isFoldCorrect:
+        run.config(state="normal")
 # fold.trace_add("read", checkFold())
 
 foldEntry = tk.Entry(textvariable=fold)
@@ -115,4 +129,14 @@ loadButton = tk.Button(
 )
 loadButton.place(x = 500, y = 200)
 
+def onCLickRun():
+    checkMessage.insert("end", "")
+    checkMessage.insert("end", "")
+
+run = tk.Button(
+    text = "Run",
+    state= "disabled",
+    command= onCLickRun
+)
+run.place(x = 100, y = 450)
 root.mainloop()
